8eec8a7546b8669046351ee1c03ac00222a588c6
dfrossouw <dfrossouw@users.noreply.github.com>
2025-11-12 14:11:33 +0000

    chore: update SDK from latest Paystack OpenAPI spec

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
-// Generated from: 2025-11-03T18:18:08.580Z
+// Generated from: 2025-11-12T14:11:10.416Z
 
 // Import everything from the generated SDK
 import {
